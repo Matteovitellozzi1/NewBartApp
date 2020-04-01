fae85a2dc9cabfea2a91eb2404e3e6b5cf2d1358
msospetti <mattiasospetti@libero.it>
2020-04-01 19:09:23 +0200

    profilo (no cambio icona)

---
+++ app/src/main/java/com/univpm/bartapp/HomeScreen.java
@@ -0,0 +1,32 @@
+package com.univpm.bartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class HomeScreen extends AppCompatActivity {
+
+    Menu menu= findViewById(R.id.profilo);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public void profilo(MenuItem item){
+        Intent intent= new Intent(this, Profilo.class);
+        startActivity(intent);
+    }
+}
