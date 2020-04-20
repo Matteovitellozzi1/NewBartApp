2dfae590644fe29231e3664123f40657912cec99
msospetti <mattiasospetti@libero.it>
2020-04-20 22:25:26 +0200

    - Nuova Icona - aggiunto inputType alla mail - implementato "Contattaci" - implementato il Listener per il NavBar (DA COMPLETARE).

--- app/src/main/java/com/univpm/bartapp/HomeScreen.java
+++ app/src/main/java/com/univpm/bartapp/HomeScreen.java
@@ -1,10 +1,13 @@
 package com.univpm.bartapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,15 +15,40 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toolbar;
 
-public class HomeScreen extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
+    MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        menuItem= (MenuItem) findViewById(R.id.Contattaci); //N.B.: NON SO SE SERVA VERAMENTE
+
+        Log.i("a", "SONO QUI"); // ricordati di cancellare tutti i Log
+
+        NavigationView navView = (NavigationView) findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) { // con uno switch potrò scegliere cosa fare in base a cosa premuto
+
+        Log.i("a", "MESSAGGIO");
+
+        invioMail();
+        return true;
+    }
+
+    public void invioMail(){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Help Request");
+        //intent.putExtra(Intent.EXTRA_TEXT, et.getText().toString());
+        intent.setData(Uri.parse("mailto:mattiasospetti@libero.it"));
+        startActivity(intent);
+    }
 }
 
