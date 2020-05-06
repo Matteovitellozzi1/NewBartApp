0cdba133423a09cfcb8a867f945769088458b9c3
nclmr <s1083490@studenti.univpm.it>
2020-05-06 22:52:24 +0200

    Main Activity modificata.

--- app/src/main/java/com/univpm/bartapp/MainActivity.java
+++ app/src/main/java/com/univpm/bartapp/MainActivity.java
@@ -7,17 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button objButton= (Button) findViewById(R.id.button2);
+        SchermataLogin();
     }
-    public void SchermataLogin(View v){
-        Intent intent= new Intent(this, Login_Screen.class);
-        startActivity(intent);
+    public void SchermataLogin(){
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            Intent intent = new Intent(this, HomeScreen.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, Login_Screen.class);
+            startActivity(intent);
+        }
     }
 
 
